cc79d4ab7e01a0dd36478f6214194ab3a8200f68
A <karrar3035@gmail.com>
2025-11-05 19:37:17 +0300

    project should be done

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,23 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return 0;
+  if (numbers.length === 1) return numbers[0];
+  return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  if (numbers.length === 0) return NaN;
+  if (numbers.length === 1) return numbers[0];
+  if (numbers.slice(1).some(num => num === 0)) {
+    return NaN; // Division by zero
+  }
+  return numbers.slice(1).reduce((result, num) => result / num, numbers[0]);
 }
 
